06d3faa0d164f5dc6b40b823a7940cf30033f0b7
Ryan Weiner <79421575+ryan-jordanweiner@users.noreply.github.com>
2021-02-27 18:11:42 -0500

    Create Model.py

---
+++ Model.py
@@ -0,0 +1,5 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, LSTM, Reccurent2D
