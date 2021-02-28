f7058d5141f02fa90547b1cfffaee13769d23164
Ryan Weiner <79421575+ryan-jordanweiner@users.noreply.github.com>
2021-02-28 15:38:26 -0500

    Update Model.py

--- Model.py
+++ Model.py
@@ -3,3 +3,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten, LSTM, Reccurent2D
+
+
+
+model = Sequential()
+model.add(LSTM('''add details here'''))
+model.add(Dropout(0.15))
+model.add(Conv2D('''details'''))
+model.add(Dense(128))
+model.add(Dropout(0.15))
+model.add(Flatten())
+model.add(Dense(128))
+model.add(Flatten())
+
+model.compile(optimizer = adam)
+model.fit()
